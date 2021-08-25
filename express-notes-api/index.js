8753474d31c2805f06ca29d3283c46c7891c4953
Fernando Figueiredo <bookaneers@gmail.com>
2021-08-25 15:28:03 +0000

    Completed GET a single note.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,13 +1,16 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
 
 // GET a list of notes =====
-var fs = require('fs');
-fs.readFile('data.json', 'utf8', (err, data) => {
-  if (err) throw err;
-  app.get('/api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
+  var fs = require('fs');
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const arrayOfData = [];
     const database = JSON.parse(data);
-    arrayOfData = [];
     for (var key in database.notes) {
       arrayOfData.push(database.notes[key]);
     }
@@ -17,23 +20,35 @@ fs.readFile('data.json', 'utf8', (err, data) => {
 // =====
 
 // GET a single note =====
-var fs = require('fs');
-fs.readFile('data.json', 'utf8', (err, data) => {
-  if (err) throw err;
-  app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res) => {
+  const idNumber = Number(req.params.id);
+  if (!Number.isInteger(idNumber) || idNumber <=0) {
+    return res.status(400).json({ error: 'id must be a positive integer' });
+  }
+  var fs = require('fs');
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const arrayOfData = [];
     const database = JSON.parse(data);
-    arrayOfData = [];
     for (var key in database.notes) {
       arrayOfData.push(database.notes[key]);
     }
-
     for (var i = 0; i < arrayOfData.length; i++) {
-      if (arrayOfData[i].id === req.params.id) {
-        res.status(200).send(arrayOfData[i]);
+      if (String(arrayOfData[i].id) === req.params.id) {
+        return res.status(200).send(arrayOfData[i]);
       }
     }
-    res.status(200).send(arrayOfData[1].id);
+    return res.status(400).json({ error: `cannot find note with id ${idNumber}` });
   });
+});
+// =====
+// POST a new note =====
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  const id = nextId++;
+  newNote.id = id;
+  notes[id] = newNote;
+  return res.status(201).json(newNote);
 });
 // =====
 

70de59fe67d0d9afd712d196171be872aeef6e6d
Fernando Figueiredo <bookaneers@gmail.com>
2021-09-08 05:39:16 +0000

    Almost completed POST routine.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -21,7 +21,7 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
     // so we immediately respond to the client and STOP the code
     // with a return statement
     res.status(400).json({
-      error: '"gradeId" must be a positive integer'
+      error: '"gradeId" must be a positive integer.'
     });
     return;
   }
@@ -56,7 +56,7 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
         // we could not have known ahead of time without actually querying the db
         // but the specific grade being requested was not found in the database
         res.status(404).json({
-          error: `Cannot find grade with "gradeId" ${gradeId}`
+          error: 'Cannot find grade with "gradeId" ${gradeId}.'
         });
       } else {
         // the specific grade was found in the database, yay!
@@ -96,6 +96,79 @@ app.get('/api/grades', (req, res, next) => {
       });
     });
 });
+
+
+// POST
+
+app.post('/api/grades', (req, res, next) => {
+  // validate the "inputs" FIRST
+
+  const nameBody = req.body.name;
+  const courseBody = req.body.course;
+  const scoreBody = parseInt(req.body.score, 10);
+
+
+  if ((!nameBody) || (!courseBody) || (!scoreBody)) {
+    res.status(400).json({
+      error: '"invalid grade". Missing "name", "course", or "score".'
+    });
+    return;
+  }
+
+  if (!Number.isInteger(scoreBody) || (scoreBody > 100 || scoreBody <= 0)) {
+    res.status(400).json({
+      error: '"gradeId" must be a positive integer between 0 and 100.'
+    });
+    return;
+  }
+
+  // Ok, the input is reasonable, time to query the database.
+  const sql = `
+    insert into grades (name, course, score)
+    values (nameBody, courseBody, scoreBody)
+    returning *
+  `;
+  // 👆 We are NOT putting the user input directly into our query
+  // const params = [gradeId];
+  // 👆 instead, we are sending the user input in a separate array
+  /**
+   * review the documentation on parameterized queries here:
+   * https://node-postgres.com/features/queries#parameterized-query
+   * you'll be using this information to prevent SQL injection attacks
+   *
+   * https://www.youtube.com/watch?v=_jKylhJtPmI
+   *
+   */
+
+  const params = [gradeId];
+  db.query(sql,params)
+    .then(result => {
+
+      // the specific grade was found in the database, yay!
+      const grade = result.rows[0];
+      if (!grade) {
+        // we could not have known ahead of time without actually querying the db
+        // but the specific grade being requested was not found in the database
+        res.status(404).json({
+          error: 'Cannot find grade with "gradeId" ${gradeId}.'
+        });
+      } else {
+        // the specific grade was found in the database, yay!
+        res.json(grade);
+      }
+    })
+    .catch(err => {
+      // the query failed for some reason
+      // possibly due to a syntax error in the SQL statement
+      // print the error to STDERR (the terminal) for debugging purposes
+      console.error(err);
+      // respond to the client with a generic 500 error message
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
 
 
 app.listen(3000,() => {
